3338687a7cc0db6b1a6d3ca654acc409934a9396
Gabriel-cruz209 <gc572640@gmail.com>
2025-08-01 16:36:03 -0300

    Atualizado

---
+++ Sql.sql
@@ -0,0 +1,16 @@
+
+create database pizzaria;
+
+use pizzaria;
+
+create table pizzas(
+id_pizza int primary key
+);
+
+create table clientes(
+id_cliente int primary key
+);
+
+create table vendas(
+id_venda int primary key
+);
